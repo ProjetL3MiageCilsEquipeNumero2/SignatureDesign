d65aeaba171420eed7d44694fbdcc3aaf8c92832
Ryan GHOMARI <ryaneghomari@gmail.com>
2021-03-15 03:08:17 +0100

    Update Commande.java

--- SignatureDesign/src/main/java/org/ProjetL3MiageCilsEquipeNumero2/Magasin/Commande.java
+++ SignatureDesign/src/main/java/org/ProjetL3MiageCilsEquipeNumero2/Magasin/Commande.java
@@ -1,5 +1,22 @@
 package org.ProjetL3MiageCilsEquipeNumero2.Magasin;
 
-public class Commande {
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.ProjetL3MiageCilsEquipeNumero2.SQLcommunication.DataBase;
+import org.ProjetL3MiageCilsEquipeNumero2.SignatureDesign.App;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Commande {
+    private static ObservableList<Article> commandes = FXCollections.observablearrayList();
+
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty nom_client;
 }
