92222c1b7090442d760feb4e0bbea60aa671dca6
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-15 17:14:30 +0100

    fonctionalite ajout article UI

--- SignatureDesign/src/main/java/org/ProjetL3MiageCilsEquipeNumero2/SignatureDesign/Articles_uiController.java
+++ SignatureDesign/src/main/java/org/ProjetL3MiageCilsEquipeNumero2/SignatureDesign/Articles_uiController.java
@@ -10,7 +10,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.converter.DoubleStringConverter;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableView;
@@ -25,6 +30,9 @@ public class Articles_uiController {
 	@FXML
 	private Button filtres_button;
 
+	// VBox
+	@FXML
+	private VBox affichage;
 	// table articles
 	@FXML
 	private TableView<Article> main_table;
@@ -37,21 +45,11 @@ public class Articles_uiController {
 
 	// panel article
 	@FXML
-	private Label id_label;
-	@FXML
 	private Label v_id;
-	@FXML
-	private Label nom_label;
 	@FXML
 	private Label v_nom;
 	@FXML
-	private Label prix_label;
-	@FXML
 	private Label v_prix;
-	@FXML
-	private Label marque_label;
-	@FXML
-	private Label cat_label;
 	@FXML
 	private Label v_marque;
 	@FXML
@@ -72,12 +70,39 @@ public class Articles_uiController {
 	private MenuItem modifier;
 	@FXML
 	private MenuItem nouveau;
+	@FXML
+	private MenuItem ajoutQT;
+
+	// nouveau article / modif article
+	@FXML
+	private TextField nom;
+	@FXML
+	private TextField prix;
+	@FXML
+	private TextField marque;
+	@FXML
+	private TextField categorie;
+	@FXML
+	private Button create;
+	@FXML
+	private Label msgerreur;
+	@FXML
+	private Button modif;
+
+	// Create/modif article
+	@FXML
+	private AnchorPane createpanel;
 
 	TableViewSelectionModel<Article> selectionModel = null;
 	ObservableList<Article> selection = null;
 
+	// initialisation
 	@FXML
 	public void initialize() {
+		//formatters input double (prix)
+		prix.setTextFormatter(new TextFormatter<>(new DoubleStringConverter()));
+		
+		//afficahge tables
 		id_col.setCellValueFactory(new PropertyValueFactory<Article, Integer>("id"));
 		nom_col.setCellValueFactory(new PropertyValueFactory<Article, String>("nom"));
 		prix_col.setCellValueFactory(new PropertyValueFactory<Article, Double>("prix"));
@@ -114,8 +139,9 @@ public class Articles_uiController {
 		v_cat.setText(focus.getCategorie());
 		sec_table.setItems(focus.getQuantites());
 	}
-	
-	public void panelDefault() {
+
+	// panel default
+	public void cardDefault() {
 		v_id.setText("N/A");
 		v_nom.setText("N/A");
 		v_prix.setText("N/A");
@@ -123,9 +149,8 @@ public class Articles_uiController {
 		v_cat.setText("N/A");
 		sec_table.getItems().clear();
 	}
-	
-	
-	
+
+	// supprssion article
 	@FXML
 	public void supprimer() {
 		if (!selection.isEmpty()) {
@@ -135,17 +160,65 @@ public class Articles_uiController {
 			focus.delete();
 			// actualisation de la table articles
 			Article.articlesUpdate();
-			//on vide la selection
+			// on vide la selection
 			selectionModel.clearSelection();
-			//on met à jour le panel avec des valeurs N/A car aucun article est selectionne
-			panelDefault();
+			// on met à jour le panel avec des valeurs N/A car aucun article est selectionne
+			cardDefault();
 		}
 	}
 
+	
+	public void panelDefault() {
+		nom.setText(null);
+		prix.setText(null);
+		categorie.setText(null);
+		marque.setText(null);
+		msgerreur.setVisible(false);
+	}
 	/*
 	 * ouvre un panel de création d'article
 	 */
+	@FXML
 	public void createPanel() {
+		affichage.setEffect(new GaussianBlur());
+		affichage.setDisable(true);
+		panelDefault();
+		createpanel.setVisible(true);
 
 	}
+
+	/*
+	 * cree un nouveau article
+	 * 
+	 */
+	@FXML
+	public void createArticle() {
+		if(prix.getText()==""
+				|| prix.getText()==null
+				|| nom.getText()==null
+				|| marque.getText()==null
+				|| categorie.getText()==null){
+			//prix n'est pas un chiffre
+			msgerreur.setVisible(true);
+		}else {
+			Article.createArticle(nom.getText(), Double.parseDouble(prix.getText()), marque.getText(), categorie.getText());
+			Article.articlesUpdate();
+			createpanel.setVisible(false);
+			affichage.setDisable(false);
+			affichage.setEffect(null);
+			affichage.setVisible(true);
+		}
+		
+		
+
+	}
+	
+	@FXML
+	public void annuller() {
+		createpanel.setVisible(false);
+		affichage.setDisable(false);
+		affichage.setEffect(null);
+		affichage.setVisible(true);
+	}
+
 }
